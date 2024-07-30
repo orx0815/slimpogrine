522e65e96d8d8d38cc705f46ac3100743fe8d385
dirk.tschentscher <orx0815@users.noreply.github.com>
2024-07-30 23:07:50 +0200

    Sample-content download with curl

--- content-packages/sample-content/content-download.sh
+++ content-packages/sample-content/content-download.sh
@@ -4,7 +4,15 @@ if [[ $0 == *":\\"* ]]; then
   DISPLAY_PAUSE_MESSAGE=true
 fi
 
-mvn package -Dvault.unpack=true wcmio-content-package:download
+echo "Not worky on wcmio-content-package:download. Tying curl"
+
+# curl -u admin:admin  --data-raw 'event.job.topic=com%2Fcomposum%2Fsling%2Fcore%2Fpckgmgr%2FPackageJobExecutor&reference=%2Fmotorbrot-samples%2Fslimpogrine-sample-content-1.0-SNAPSHOT.zip&_charset_=UTF-8&operation=assemble&overridePath=%2Fmotorbrot-samples%2Fslimpogrine-sample-content-1.0-SNAPSHOT.zip'
+
+curl -u admin:admin 'http://localhost:8080/bin/cpm/package.download.zip/motorbrot-samples/slimpogrine-sample-content-1.0-SNAPSHOT.zip' --output slimpogrine-sample-content-1.0-SNAPSHOT.zip
+unzip -o slimpogrine-sample-content-1.0-SNAPSHOT.zip
+rm slimpogrine-sample-content-1.0-SNAPSHOT.zip
+
+#mvn package -Dvault.unpack=true wcmio-content-package:download
 
 if [ "$DISPLAY_PAUSE_MESSAGE" = true ]; then
   echo ""
