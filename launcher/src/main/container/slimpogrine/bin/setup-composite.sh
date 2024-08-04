db1cee836c2c8c80112d93e3584d8ca991a4ebac
dirk.tschentscher <orx0815@users.noreply.github.com>
2024-08-04 13:03:47 +0200

    WIP oak-sns, docker compose

--- launcher/src/main/container/slimpogrine/bin/setup-composite.sh
+++ launcher/src/main/container/slimpogrine/bin/setup-composite.sh
@@ -4,12 +4,9 @@ SLING_USERNAME="${SLING_USERNAME:-admin}"
 SLING_PASSWORD="${SLING_PASSWORD:-admin}"
 
 echo "Creating composite seed..."
-java \
-    -Dsling.run.modes=composite-seed,${RUNMODE} \
-    -jar org.apache.sling.feature.launcher.jar \
-    -f /opt/slingcms/setup/${CMS_ARTIFACT_ID}-${FM_SEED_CLASSIFIER}.slingosgifeature \
-    -f /opt/slingcms/features/*.slingosgifeature &
-SLING_PID=$!
+
+/opt/sling/bin/launch.sh slimpogrine_composite_seed & SLING_PID=$!
+
 echo "Sling PID: ${SLING_PID}"
 
 sleep 30s
@@ -34,10 +31,10 @@ if [ $STARTED -eq 1 ]; then
     exit 2
 else
     echo "Cleaning up seeding..."
-    rm -rf /opt/slingcms/launcher/framework /opt/slingcms/launcher/logs \
-        /opt/slingcms/launcher/repository /opt/slingcms/launcher/resources \
-        /opt/slingcms/setup
-    ln -s /opt/slingcms/launcher/composite/repository-libs/segmentstore \
-        /opt/slingcms/launcher/composite/repository-libs/segmentstore-composite-mount-libs
+    rm -rf /opt/sling/launcher/framework /opt/sling/launcher/logs \
+        /opt/sling/launcher/repository /opt/sling/launcher/resources \
+        /opt/sling/setup
+    ln -s /opt/sling/launcher/composite/repository-libs/segmentstore \
+        /opt/sling/launcher/composite/repository-libs/segmentstore-composite-mount-libs
 fi
 echo "Repository seeded successfully!"
