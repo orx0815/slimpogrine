e7c1d5e40ff8adfb0252273e5f6cee58dbbc9bae
dirk.tschentscher <orx0815@users.noreply.github.com>
2025-01-24 16:04:32 +0100

    SliGRINE httpd proxy

--- launcher/clean_install_docker_run.sh
+++ launcher/clean_install_docker_run.sh
@@ -6,5 +6,5 @@ echo '--------------------------------------------------------------------------
 # docker run -p 8080:8080 -v /tmp/sling:/opt/sling/sling apache/sling:12
 
 mvn clean package -Ddocker.skip=false
-docker volume create sling-launcher
-docker run -p 8080:8080 -v sling-launcher:/opt/sling/launcher ghcr.io/orx0815/slimpogrine:snapshot
+docker volume create slimpogrine-volume
+docker run -p 8080:8080 -v slimpogrine-volume:/opt/sling/launcher ghcr.io/orx0815/slimpogrine:snapshot
---
+++ launcher/webcache_docker_build_run.sh
@@ -0,0 +1,8 @@
+
+echo '-------------------------------------------------------------------------------------------'
+echo '[NOTE] Launching Slimpogrine webcache conatiner'
+echo '-------------------------------------------------------------------------------------------'
+
+docker build --tag 'ghcr.io/orx0815/slimpogrine_webcache:latest' ./src/main/container/webcache
+#docker volume create slimpogrine-volume
+docker run -t -i -p 80:80  -v slimpogrine-volume:/opt/sling/launcher --env RENDERER_URL=172.17.0.1 --env AUTHOR_URL=172.17.0.1 ghcr.io/orx0815/slimpogrine_webcache:latest
