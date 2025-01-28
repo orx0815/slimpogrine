5f4384fa521eaccfff3ba01f66bd6c2765064068
dirk.tschentscher <orx0815@users.noreply.github.com>
2025-01-28 17:11:17 +0100

    Polish sling-archetype no WYSIWYG core app by csszengarden.com Commit to their markup, but read texts from repo (in future)
    https://github.com/mezzoblue/csszengarden.com

---
+++ apps/core.example/core/src/main/java/org/motorbrot/slimpogrine/slingmodels/CssZenGarden.java
@@ -0,0 +1,32 @@
+package org.motorbrot.slimpogrine.slingmodels;
+
+import java.util.Random;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Answers with css id
+ */
+@Model(adaptables = {SlingHttpServletRequest.class})
+public class CssZenGarden {
+
+  private final String[] CSS_IDS = new String[]{"099", "142", "214", "215", "219"};
+
+  private static final Logger LOG = LoggerFactory.getLogger(SampleRequestModel.class);
+
+  private String cssId;
+
+  public CssZenGarden() {
+    Random rand = new Random();
+    this.cssId = CSS_IDS[rand.nextInt(CSS_IDS.length)];
+    LOG.info("CSS-id is: " + this.cssId);
+  }
+
+  public String getRandomCss() {
+    return this.cssId;
+  }
+
+}
