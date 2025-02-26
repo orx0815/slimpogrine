ad6a30a443f8c17362edb6cd0d75f79afcfb7dfd
dirk.tschentscher <orx0815@users.noreply.github.com>
2025-02-26 20:24:49 +0100

    Clarify that "Download not support for Composum yet with wcmio-content-package"

--- content-packages/sample-content-sligrine/content-download.sh
+++ content-packages/sample-content-sligrine/content-download.sh
@@ -4,9 +4,9 @@ if [[ $0 == *":\\"* ]]; then
   DISPLAY_PAUSE_MESSAGE=true
 fi
 
-echo "Not worky on wcmio-content-package:download. Tying curl"
+# https://github.com/wcm-io/io.wcm.maven.plugins.wcmio-content-package-maven-plugin/blob/develop/src/it/sling-composum-upload-download/pom.xml#L192
 
-# curl -u admin:admin  --data-raw 'event.job.topic=com%2Fcomposum%2Fsling%2Fcore%2Fpckgmgr%2FPackageJobExecutor&reference=%2Fmotorbrot-samples%2Fslimpogrine-sample-content-1.0-SNAPSHOT.zip&_charset_=UTF-8&operation=assemble&overridePath=%2Fmotorbrot-samples%2Fslimpogrine-sample-content-1.0-SNAPSHOT.zip'
+echo "Download not support for Composum yet with wcmio-content-package:download. Using curl. You need to manually rebuild package in http://localhost:8080/bin/packages.html/motorbrot-samples/slimpogrine-sample-content-sligrine-1.0-SNAPSHOT.zip"
 
 curl -u admin:admin 'http://localhost:8080/bin/cpm/package.download.zip/motorbrot-samples/slimpogrine-sample-content-sligrine-1.0-SNAPSHOT.zip' --output slimpogrine-sample-content-sligrine-1.0-SNAPSHOT.zip
 unzip -o slimpogrine-sample-content-sligrine-1.0-SNAPSHOT.zip
