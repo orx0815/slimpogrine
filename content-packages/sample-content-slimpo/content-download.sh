236de4b0394fbf67c61fb9047d9446671089094d
dirk.tschentscher <orx0815@users.noreply.github.com>
2025-03-08 22:04:10 +0100

    wcmio-content-package-maven-plugin with download support for Composum

--- content-packages/sample-content-slimpo/content-download.sh
+++ content-packages/sample-content-slimpo/content-download.sh
@@ -5,14 +5,12 @@ if [[ $0 == *":\\"* ]]; then
 fi
 
 # https://github.com/wcm-io/io.wcm.maven.plugins.wcmio-content-package-maven-plugin/blob/develop/src/it/sling-composum-upload-download/pom.xml#L192
+# echo "Download not support for Composum yet with wcmio-content-package:download. Using curl. You need to manually rebuild package in http://localhost:8080/bin/packages.html/motorbrot-samples/slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip"
+#curl -u admin:admin 'http://localhost:8080/bin/cpm/package.download.zip/motorbrot-samples/slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip' --output slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
+#unzip -o slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
+#rm slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
 
-echo "Download not support for Composum yet with wcmio-content-package:download. Using curl. You need to manually rebuild package in http://localhost:8080/bin/packages.html/motorbrot-samples/slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip"
-
-curl -u admin:admin 'http://localhost:8080/bin/cpm/package.download.zip/motorbrot-samples/slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip' --output slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
-unzip -o slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
-rm slimpogrine-sample-content-slimpo-1.0-SNAPSHOT.zip
-
-#mvn package -Dvault.unpack=true wcmio-content-package:download
+mvn -D vault.unpack=true wcmio-content-package:download
 
 if [ "$DISPLAY_PAUSE_MESSAGE" = true ]; then
   echo ""
